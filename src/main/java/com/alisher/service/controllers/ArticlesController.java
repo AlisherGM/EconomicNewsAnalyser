c10cf1e98be7d7e74236e74cf031a6103f85dd00
dinar.rafikov <rafteri98@gmail.com>
2018-10-30 23:05:26 +0300

    add lombok

--- src/main/java/com/alisher/service/controllers/ArticlesController.java
+++ src/main/java/com/alisher/service/controllers/ArticlesController.java
@@ -3,6 +3,7 @@ package com.alisher.service.controllers;
 import com.alisher.service.dao.JdbcArticleDAO;
 import com.alisher.service.pojo.Article;
 import com.alisher.service.service.DBEditor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -16,12 +17,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 @Controller
+@RequiredArgsConstructor
 public class ArticlesController {
-    @Autowired
-    private DBEditor dbEditor;
-
-    @Autowired
-    private JdbcArticleDAO jdbcArticleDAO;
+    private final DBEditor dbEditor;
+    private final JdbcArticleDAO jdbcArticleDAO;
 
     @GetMapping("/index")
     public String getHomePage(ModelMap model) {
@@ -40,7 +39,7 @@ public class ArticlesController {
     public String getAllPhrasesByDatetime(@RequestBody String date, ModelMap model){
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime now = LocalDateTime.now();
-        String to = dtf.format(now).toString();
+        String to = dtf.format(now);
         String from = date.substring(10,20);
         System.out.println(to + " " + from);
         ArrayList<Article> list = dbEditor.getRequestArticle(from, to);
--- src/main/java/com/alisher/service/pojo/Source.java
+++ src/main/java/com/alisher/service/pojo/Source.java
@@ -1,33 +1,15 @@
 package com.alisher.service.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Source {
     private String id;
     private String name;
-
-    public Source(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Source() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
